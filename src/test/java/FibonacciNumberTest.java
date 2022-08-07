dcde2e89d40e509aa816a6fcd2283aa49d48c6fe
Corrington <RobertCorrington2@gmail.com>
2022-08-07 07:23:46 -0700

    Updated the Fibonacci sequence problem tests.

--- src/test/java/FibonacciNumberTest.java
+++ src/test/java/FibonacciNumberTest.java
@@ -7,18 +7,51 @@ public class FibonacciNumberTest {
     @Test
     public void testFib() {
 
-        var p = new FibonacciNumber();
+        var obj = new FibonacciNumber();
 
-        assertEquals(p.fib(0), 0);
-        assertEquals(p.fib(1), 1);
-        assertEquals(p.fib(2), 1);
-        assertEquals(p.fib(3), 2);
-        assertEquals(p.fib(4), 3);
-        assertEquals(p.fib(5), 5);
-        assertEquals(p.fib(6), 8);
-        assertEquals(p.fib(7), 13);
-        assertEquals(p.fib(8), 21);
-        assertEquals(p.fib(9), 34);
+        assertEquals(obj.fib1(0), 0);
+        assertEquals(obj.fib1(1), 1);
+        assertEquals(obj.fib1(2), 1);
+        assertEquals(obj.fib1(3), 2);
+        assertEquals(obj.fib1(4), 3);
+        assertEquals(obj.fib1(5), 5);
+        assertEquals(obj.fib1(6), 8);
+        assertEquals(obj.fib1(7), 13);
+        assertEquals(obj.fib1(8), 21);
+        assertEquals(obj.fib1(9), 34);
+
+        assertEquals(obj.fib2(0), 0);
+        assertEquals(obj.fib2(1), 1);
+        assertEquals(obj.fib2(2), 1);
+        assertEquals(obj.fib2(3), 2);
+        assertEquals(obj.fib2(4), 3);
+        assertEquals(obj.fib2(5), 5);
+        assertEquals(obj.fib2(6), 8);
+        assertEquals(obj.fib2(7), 13);
+        assertEquals(obj.fib2(8), 21);
+        assertEquals(obj.fib2(9), 34);
+
+        assertEquals(obj.fib3(0), 0);
+        assertEquals(obj.fib3(1), 1);
+        assertEquals(obj.fib3(2), 1);
+        assertEquals(obj.fib3(3), 2);
+        assertEquals(obj.fib3(4), 3);
+        assertEquals(obj.fib3(5), 5);
+        assertEquals(obj.fib3(6), 8);
+        assertEquals(obj.fib3(7), 13);
+        assertEquals(obj.fib3(8), 21);
+        assertEquals(obj.fib3(9), 34);
+
+        assertEquals(obj.fib4(0), 0);
+        assertEquals(obj.fib4(1), 1);
+        assertEquals(obj.fib4(2), 1);
+        assertEquals(obj.fib4(3), 2);
+        assertEquals(obj.fib4(4), 3);
+        assertEquals(obj.fib4(5), 5);
+        assertEquals(obj.fib4(6), 8);
+        assertEquals(obj.fib4(7), 13);
+        assertEquals(obj.fib4(8), 21);
+        assertEquals(obj.fib4(9), 34);
 
     } // testFib()
 
