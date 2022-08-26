2a9a333df4f41410525de9f4c03da99943276005
Corrington <RobertCorrington2@gmail.com>
2022-08-25 17:03:01 -0700

    Solved 383. Ransom Note

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,7 +1,48 @@
+/*
+Problem 383. Ransom Note
+https://leetcode.com/problems/ransom-note/
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed
+by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+    Input: ransomNote = "a", magazine = "b"
+    Output: false
+
+Example 2:
+    Input: ransomNote = "aa", magazine = "ab"
+    Output: false
+
+Example 3:
+    Input: ransomNote = "aa", magazine = "aab"
+    Output: true
+
+Constraints:
+    1 <= ransomNote.length, magazine.length <= 105
+    ransomNote and magazine consist of lowercase English letters.
+ */
 public class RansomNote {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        return false;
+        // [0] = number of 'a's, [1] = number of 'b's. etc.
+        char[] countsOfLettersInMagazine = new char[26];
+
+        // determine number of each lowercase letter in the magazine
+        for (var letter : magazine.toCharArray()) {
+            int letterIndex = letter - 'a';
+            countsOfLettersInMagazine[ letterIndex ]++;
+        } // for
+
+        // ensure we have enough of each letter to complete the ransom note
+        for (var letter : ransomNote.toCharArray()) {
+            int letterIndex = letter - 'a';
+            if (countsOfLettersInMagazine[ letterIndex ] == 0) return false;
+            countsOfLettersInMagazine[ letterIndex ]--;
+        } // for
+
+        return true;
+
     } // canConstruct()
 
 } // class RansomNote
--- src/test/java/RansomNoteTest.java
+++ src/test/java/RansomNoteTest.java
@@ -1,8 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class RansomNoteTest {
 
     @Test
     void canConstructTest() {
+
+        var obj = new RansomNote();
+
+        assertFalse(obj.canConstruct("a", "b" ));
+        assertFalse(obj.canConstruct("aa", "ab" ));
+        assertTrue(obj.canConstruct("aa", "aab" ));
+
+        assertTrue(obj.canConstruct("aa", "abcabc" ));
+        assertTrue(obj.canConstruct("xyz", "xyz" ));
+        assertTrue(obj.canConstruct("abcxyz", "xyzabc" ));
+        assertFalse(obj.canConstruct("abc", "xyz" ));
+        assertTrue(obj.canConstruct("z", "abcdefghijklmnopqrstuvwxyz" ));
+
     } // canConstructTest()
+
 } // class RansomNoteTest
